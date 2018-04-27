54a8cd424fd3154796ca79c2d6afdfa7a79ba043
YeJunKim <sillon2808@gmail.com>
2018-04-27 23:40:44 +0900

    <This is Django Test!>

---
+++ djangogirls/blog/models.py
@@ -0,0 +1,50 @@
+
+
+from django.db import models
+from django.utils import timezone
+
+"""
+models.CharField - Used to define text with a limited number of characters. Use this to store short string information, such as a title.
+
+models.TextField - Property for long text with no limit on the number of characters.
+
+models.DateTimeField - Represents the date and time.
+
+models.ForeignKey - Links to other models.
+"""
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
